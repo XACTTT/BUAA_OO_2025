38ef25ec84526a8318248edec1a78e79e3bc67c4
欧阳志豪 <23371423@buaa.edu.cn>
2025-03-11 19:09:31 +0800

    不修合并

--- src/Poly.java
+++ src/Poly.java
@@ -295,7 +295,9 @@ public class Poly {
         for (Unit unit : this.poly) {
             int sign2 = 0;
             for (Unit unit1 : p.poly) {
-                if (unit.equals(unit1) && unit.getCoe().equals(unit1.getCoe())) {
+                if (unit.equals(unit1)
+                     //   && unit.getCoe().equals(unit1.getCoe())
+                ) {
                     sign2 = 1;
                 }
             }
@@ -308,7 +310,9 @@ public class Poly {
         for (Unit unit : p.poly) {
             int sign4 = 0;
             for (Unit unit1 : this.poly) {
-                if (unit.equals(unit1) && unit.getCoe().equals(unit1.getCoe())) {
+                if (unit.equals(unit1)
+                   //     && unit.getCoe().equals(unit1.getCoe())
+                ) {
                     sign4 = 1;
                 }
             }
