e6151074ceae0b9b64af204563e3cb32c3042ae7
欧阳志豪 <23371423@buaa.edu.cn>
2025-05-14 17:59:08 +0800

    final+check2+junit

--- src/Person.java
+++ src/Person.java
@@ -234,7 +234,7 @@ public class Person implements PersonInterface {
     }
 
     public void removeArticle(int id) {
-        receivedArticles.remove((Integer) id);
+        receivedArticles.removeIf(e -> e.equals(id));
     }
 
 }
